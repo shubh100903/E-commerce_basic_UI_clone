964a11e5bbf29bb4d564bc555896d16d8104486f
Sumit Yadav <151856501+shubh100903@users.noreply.github.com>
2025-11-27 20:11:51 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_windows_3.32.6-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\cursor_ai_app_developer\\my_ai_app_projects\\e_commerce_ui" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_windows_3.32.6-stable\\flutter"
+  "PROJECT_DIR=D:\\cursor_ai_app_developer\\my_ai_app_projects\\e_commerce_ui"
+  "FLUTTER_ROOT=C:\\flutter_windows_3.32.6-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\cursor_ai_app_developer\\my_ai_app_projects\\e_commerce_ui\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\cursor_ai_app_developer\\my_ai_app_projects\\e_commerce_ui"
+  "FLUTTER_TARGET=D:\\cursor_ai_app_developer\\my_ai_app_projects\\e_commerce_ui\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuNw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZDdiNTIzYjM1Ng==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MzlkNmQ2ZTY5OQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\cursor_ai_app_developer\\my_ai_app_projects\\e_commerce_ui\\.dart_tool\\package_config.json"
+)
